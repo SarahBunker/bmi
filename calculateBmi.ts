ef9048c62acf9d9b0760cf747fd616341d9fb182
Sarah Bunker <ChemEnCoder@gmail.com>
2022-12-05 09:47:14 -0700

    first commit

---
+++ calculateBmi.ts
@@ -0,0 +1,47 @@
+function calculateBmi(height:number, weight:number) {
+  let heightm = height / 100;
+  let bmi = weight / ( heightm * heightm)
+  if (bmi < 16) return "Underweight - severe thinness"
+  if (bmi < 16.9) return "Underweight - moderate thinness"
+  if (bmi < 18.4) return "Underweight - mild thinness"
+  if (bmi < 24.9) return "Normal range"
+  if (bmi < 29.9) return "Overweight - pre-obese"
+  if (bmi < 34.9) return "Obese - Class I"
+  if (bmi < 39.9) return "Obese - Class II"
+  return "Obese - Class III"
+}
+
+// console.log(calculateBmi(180, 74))
+
+interface MultipleValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>) => {
+  args = args.slice(2)
+  if (args.length < 2) throw new Error('Not enough arguments');
+  if (args.length > 2) throw new Error('Too many arguments');
+
+  args.forEach((arg: string) => {
+    if (isNaN(Number(arg))) throw new Error('Provided values were not numbers!');
+  });
+  let argsNumber = args.map( (arg: string) => Number(arg));
+  console.log(argsNumber);
+
+  return {
+    height: argsNumber[0],
+    weight: argsNumber[1]
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
---
+++ exerciseCalculator.ts
@@ -0,0 +1,76 @@
+interface ExerciseResults {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const exerciseCalculator = (hours: Array<number>, target: number): ExerciseResults => {
+  let periodLength = hours.length;
+  let trainingDays = hours.filter( hour => hour > 0).length;
+
+  let totalhours = hours.reduce( (total, current) => total += current);
+  let average = totalhours/periodLength;
+
+  let rating = 0
+  if (trainingDays/periodLength > 0.7) rating += 1;
+  if (average > 1.75) rating += 1;
+  if (average > 2) rating += 1;
+  let success = (rating >= target);
+
+  let ratingDescription = "ooff this was a bad week";
+  switch (rating) {
+    case 1:
+      ratingDescription = "great job for trying";
+      break;
+    case 2:
+      ratingDescription = "you made your goal";
+      break;
+    case 3:
+      ratingDescription = "wow, you are killing it this week";
+      break;
+    default:
+  }
+  console.log(ratingDescription);
+  return {
+    periodLength, trainingDays, success, rating, ratingDescription, target, average
+  }
+}
+
+// console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+interface MultipleValues {
+  array: Array<number>;
+  target: number;
+}
+
+const parseArguments = (args: Array<string>) => {
+  args = args.slice(2)
+  if (args.length < 2) throw new Error('Not enough arguments');
+  // if (args.length > 8) throw new Error('Too many arguments');
+
+  args.forEach((arg: string) => {
+    if (isNaN(Number(arg))) throw new Error('Provided values were not numbers!');
+  });
+  let argsNumber = args.map( (arg: string) => Number(arg));
+  console.log(argsNumber);
+
+  return {
+    target: argsNumber[0],
+    array: argsNumber.slice(1)
+  }
+}
+
+try {
+  const { target, array } = parseArguments(process.argv);
+  console.log(exerciseCalculator(array, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
